fb017729dfda3903b904cdfb1f88f42ce1ac165c
Clemens Prescher <clemens.prescher@gmail.com>
2015-08-06 14:12:24 -0500

    change iterator, to now do all files...

--- analyze_rdf_data.py
+++ analyze_rdf_data.py
@@ -34,41 +34,45 @@ conditions_df = pd.read_csv(os.path.join(output_folder, "conditions.csv"),
 sq_weighted_series_df = pd.DataFrame()
 rdf_weighted_series_df = pd.DataFrame()
 
-for id in range(file_numbers[0], file_numbers[1]+1):
-    filename = "RDFDAT-{}".format(id)
-    print(filename)
-    atomic_density = np.array(conditions_df["atomic_density"])[id-1]
-    pressure = np.array(conditions_df["pressure (GPa)"])[id-1]
+files = os.listdir(folder_name)
+output_path = create_folder(output_folder, "conditions")
+
+for filename in files:
+    if "RDFDAT-" in filename:
+        print(filename)
+        id = int(filename[7:])
+        atomic_density = np.array(conditions_df["atomic_density"])[id-1]
+        pressure = np.array(conditions_df["pressure (GPa)"])[id-1]
 
 
-    rdf_df = read_rdf_data(os.path.join(folder_name, filename))
-    rdf_df.to_csv(os.path.join(atomic_rdf_path, filename + "_atomic_rdf.csv"))
+        rdf_df = read_rdf_data(os.path.join(folder_name, filename))
+        rdf_df.to_csv(os.path.join(atomic_rdf_path, filename + "_atomic_rdf.csv"))
 
-    sq_df = calculate_atomic_sq(rdf_df, atomic_density=atomic_density)
-    sq_df.to_csv(os.path.join(atomic_sq_path, filename + "_atomic_sq.csv"))
+        sq_df = calculate_atomic_sq(rdf_df, atomic_density=atomic_density)
+        sq_df.to_csv(os.path.join(atomic_sq_path, filename + "_atomic_sq.csv"))
 
-    sq_weighted_df = calculate_combined_sq(sq_df, elemental_abundances)
-    sq_weighted_df.to_csv(os.path.join(weighted_sq_path, filename + "_combined_sq.csv"))
+        sq_weighted_df = calculate_combined_sq(sq_df, elemental_abundances)
+        sq_weighted_df.to_csv(os.path.join(weighted_sq_path, filename + "_combined_sq.csv"))
 
-    fr_weighted_df = calculate_weighted_fr(sq_weighted_df, q_max=12)
-    fr_weighted_df.to_csv(os.path.join(weighted_fr_path, filename + '_weighted_fr.csv'))
+        fr_weighted_df = calculate_weighted_fr(sq_weighted_df, q_max=12)
+        fr_weighted_df.to_csv(os.path.join(weighted_fr_path, filename + '_weighted_fr.csv'))
 
-    rdf_weighted_df = calculate_weighted_rdf(fr_weighted_df, atomic_density=atomic_density)
-    rdf_weighted_df.to_csv(os.path.join(weighted_rdf_path, filename + '_weighted_rdf.csv'))
+        rdf_weighted_df = calculate_weighted_rdf(fr_weighted_df, atomic_density=atomic_density)
+        rdf_weighted_df.to_csv(os.path.join(weighted_rdf_path, filename + '_weighted_rdf.csv'))
 
 
-    # creating the plots
-    create_df_plot(rdf_df, os.path.join(atomic_rdf_path, filename + "_atomic_rdf.png"))
-    create_df_plot(sq_df, os.path.join(atomic_sq_path, filename + "_atomic_sq.png"))
-    create_df_plot(sq_weighted_df, os.path.join(weighted_sq_path, filename + "_weighted_sq.png"))
-    create_df_plot(fr_weighted_df, os.path.join(weighted_fr_path, filename + "_weighted_fr.png"), columns="sum")
-    create_df_plot(rdf_weighted_df, os.path.join(weighted_rdf_path, filename + "_weighted_rdf.png"), columns="sum")
+        # creating the plots
+        create_df_plot(rdf_df, os.path.join(atomic_rdf_path, filename + "_atomic_rdf.png"))
+        create_df_plot(sq_df, os.path.join(atomic_sq_path, filename + "_atomic_sq.png"))
+        create_df_plot(sq_weighted_df, os.path.join(weighted_sq_path, filename + "_weighted_sq.png"))
+        create_df_plot(fr_weighted_df, os.path.join(weighted_fr_path, filename + "_weighted_fr.png"), columns="sum")
+        create_df_plot(rdf_weighted_df, os.path.join(weighted_rdf_path, filename + "_weighted_rdf.png"), columns="sum")
 
-    sq_weighted_series_df["q"] = np.array(sq_weighted_df.index.values)
-    sq_weighted_series_df[str(pressure)] = np.array(sq_weighted_df["sum"])
+        sq_weighted_series_df["q"] = np.array(sq_weighted_df.index.values)
+        sq_weighted_series_df[str(pressure)] = np.array(sq_weighted_df["sum"])
 
-    rdf_weighted_series_df["r"] = rdf_weighted_df.index.values
-    rdf_weighted_series_df[str(pressure)] = np.array(rdf_weighted_df["sum"])
+        rdf_weighted_series_df["r"] = rdf_weighted_df.index.values
+        rdf_weighted_series_df[str(pressure)] = np.array(rdf_weighted_df["sum"])
 
 
 sq_weighted_series_df = sq_weighted_series_df.set_index("q")
